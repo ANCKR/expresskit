f43fe93d75f881f68e9fdf9df800db389611b9c5
mayank-ankr <mayank.ghildiyal@xotiv.com>
2024-07-30 18:52:28 +0530

    changes in the app.ts for cors

--- src/app.ts
+++ src/app.ts
@@ -49,7 +49,8 @@ const port = 3000;
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
