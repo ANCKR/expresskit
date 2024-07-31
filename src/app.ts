56fc32df65760feb7467a24894b44822b1c028fb
mayank-ankr <mayank.ghildiyal@xotiv.com>
2024-07-31 11:38:36 +0530

    changes in the app.use function for cookies

--- src/app.ts
+++ src/app.ts
@@ -60,6 +60,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", allowedOrigins);
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Expose-Headers", ["set-cookie"]);
   next();
 });
 
